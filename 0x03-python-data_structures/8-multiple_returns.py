6821841a0b37f2f3ab748e16878455b9a8214420
eliaset <eliasshamil@gmail.com>
2023-11-15 06:38:28 -0800

    u

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@ def multiple_returns(sentence):
     my_t = ()
     l = len(sentence)
     if l == 0:
-        my_t = l, None
+        my_t = 0, None
     else:
         my_t =  l, sentence[0]
     return my_t
