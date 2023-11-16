06ed4561491267ae50089af90dff419ea35ee3f0
eliaset <eliasshamil@gmail.com>
2023-11-16 02:42:30 -0800

    u

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    l = len(my_list)
+    max = 0
+    if l == 0:
+        return None
+    else:
+        for i in my_list:
+            if i > max:
+                max = i
+        return max
