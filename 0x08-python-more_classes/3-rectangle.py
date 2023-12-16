9e788f21efda2010f9620f8284640a2ab1c4f332
Elias <eliasshamil@gmail.com>
2023-12-16 04:18:21 -0800

    8

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -58,10 +58,8 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return ("")
-
+        
         rect = []
         for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
-            if i != self.__height - 1:
-                rect.append("\n")
-        return ("".join(rect))
+            rect.append("#" * self.__width)
+        return ("\n".join(rect))
